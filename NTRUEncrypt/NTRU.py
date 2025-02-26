708e67c4fc069f4a3b66779491bcd01ab565b4b8
khanh1712 <97666518+khanh1712@users.noreply.github.com>
2025-02-26 15:39:18 +0700

    Update NTRU.py

--- NTRUEncrypt/NTRU.py
+++ NTRUEncrypt/NTRU.py
@@ -37,42 +37,59 @@ class NTRU:
 
         return self.h
     
-    def encrypt(self, m):
-
-        f_m = message_2_poly(m, self.Rp)
+    def encrypt_poly(self, f_m: Poly) -> Poly:
         r = random_ternary_list(self.d, self.d, self.N - 1)
 
         e = (Poly(self.Rq, r) * self.Rp).mul_mod_convolution_ring( self.h, self.N) + f_m.change_ring(self.Rq)
 
         return e
     
-    def decrypt(self, e):
+    def decrypt_poly(self, e: Poly) -> Poly:
         temp = Poly(self.Rq, self.f).mul_mod_convolution_ring(e, self.N) 
         
         center = temp.center_lift()
         f_m = Poly(self.Rp, center).mul_mod_convolution_ring(self.Fp, self.N)
 
-        return poly_2_message(f_m)
+        return f_m
 
-def test():
-    #Moderate security parameter
-    N = 107
-    p = 3
-    q = 64
-    df = 14
-    dg = 12
-    d = 5
+    def encrypt(self, m_list: np.ndarray[np.complex128]) -> np.ndarray[np.complex128]:
+        poly_list = complex_array_2_polys(m_list, self.Rp)
+        enc_poly = []
+        
+        for p in poly_list:
+            enc_poly.append(self.encrypt_poly(p))
+        
+        return polys_2_complex_array(enc_poly)
+    
+    def decrypt(self, enc_list: np.ndarray[np.complex1280]) -> np.ndarray[np.complex128]:
+        enc_poly = complex_array_2_polys(enc_list, self.Rp)
+        
+        m_list = []
+        
+        for p in enc_poly:
+            m_list.append(self.decrypt_poly(p))
+        
+        return polys_2_complex_array(m_list)
+         
+# def test():
+#     #Moderate security parameter
+#     N = 107
+#     p = 3
+#     q = 64
+#     df = 14
+#     dg = 12
+#     d = 5
 
-    m = b'Minh ngu vai loz'
-    t = time.time()
-    ntru = NTRU(N, p, q, df, dg, d)
-    ntru.key_gen()
-    e = ntru.encrypt(m)
-    m_ = ntru.decrypt(e)
-    print(time.time() - t)
-    assert(m == m_)
+#     m = b'Minh ngu vai loz'
+#     t = time.time()
+#     ntru = NTRU(N, p, q, df, dg, d)
+#     ntru.key_gen()
+#     e = ntru.encrypt(m)
+#     m_ = ntru.decrypt(e)
+#     print(time.time() - t)
+#     assert(m == m_)
 
-for i in range(100):
-    print(i)
-    test()
+# for i in range(100):
+#     print(i)
+#     test()
 
